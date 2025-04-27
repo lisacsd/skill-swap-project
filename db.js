83bd34f0c55051afb7f13b30b2b766181631e0b6
Lizab48 <129211040+Lizab48@users.noreply.github.com>
2025-04-27 19:59:23 +0200

    pour seveur

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',              // ou le nom que vous utilisez
-  password: 'Toccata565',              // mot de passe de Workbench
+  password: 'azerty',              // mot de passe de Workbench
   database: 'skillswap'
 });
 
